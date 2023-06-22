d807fa6b82f36f3cdf6f0fc2f77f2ac73f9eeabe
john assefa <122903268+johnassefatheeth@users.noreply.github.com>
2023-06-22 11:56:00 -0700

    removing unwanted imports

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
 from joblib import dump, load
 
