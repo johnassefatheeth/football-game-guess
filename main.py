2507e68470391cc947f1fd2ae550bdcb217d34ad
johnassefatheeth <johnassefaforbussines@gmail.com>
2023-07-07 01:03:55 -0700

    ...

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 from joblib import dump, load
 
 # Step 1: Load and Prepare the Dataset
@@ -38,61 +39,68 @@ if option == "Specific Game":
     away_team = st.text_input("Enter the name of the away team:")
 
     if st.button("Predict"):
+        if home_team not in df['HomeTeam'].values or away_team not in df['AwayTeam'].values:
+            st.write("Invalid team names. Please try again.")
+        else:
+            # Prepare input for prediction
+            home_team_data = df[df['HomeTeam'] == home_team].tail(1)
+            away_team_data = df[df['AwayTeam'] == away_team].tail(1)
 
-                if home_team not in df['HomeTeam'].values or away_team not in df['AwayTeam'].values:
-                    print("Invalid team names. Please try again.")
-                else:
-                    # Prepare input for prediction
-                    home_team_data = df[df['HomeTeam'] == home_team].tail(1)
-                    away_team_data = df[df['AwayTeam'] == away_team].tail(1)
+            new_data = pd.DataFrame({'AvgHS': home_team_data['AvgHS'].values,
+                                     'AvgAS': away_team_data['AvgAS'].values,
+                                     'AvgHST': home_team_data['AvgHST'].values,
+                                     'AvgAST': away_team_data['AvgAST'].values})
 
-                    new_data = pd.DataFrame({'AvgHS': home_team_data['AvgHS'].values,
-                                             'AvgAS': away_team_data['AvgAS'].values,
-                                             'AvgHST': home_team_data['AvgHST'].values,
-                                             'AvgAST': away_team_data['AvgAST'].values})
+            # Make predictions
+            home_goals = round(model_home_goals.predict(new_data)[0])
+            away_goals = round(model_away_goals.predict(new_data)[0])
 
-                    # Make predictions
-                    home_goals = round(model_home_goals.predict(new_data)[0])
-                    away_goals = round(model_away_goals.predict(new_data)[0])
+            # Calculate evaluation metrics
+            home_goals_pred = model_home_goals.predict(X)
+            away_goals_pred = model_away_goals.predict(X)
+            mse_home = mean_squared_error(y_home_goals, home_goals_pred)
+            mse_away = mean_squared_error(y_away_goals, away_goals_pred)
+            r2_home = r2_score(y_home_goals, home_goals_pred)
+            r2_away = r2_score(y_away_goals, away_goals_pred)
 
-                    # Step 5: Display Prediction Result
-                    # print("Prediction:")
-                    st.write(f"Prediction: {home_team} {home_goals} : {away_goals} {away_team}")
+            # Step 5: Display Prediction Result and Evaluation Metrics
+            st.write(f"Prediction: {home_team} {home_goals} : {away_goals} {away_team}")
+            st.write(f"MSE for home goals: {mse_home:.2f}")
+            st.write(f"MSE for away goals: {mse_away:.2f}")
+            st.write(f"R-squared score for home goals: {r2_home:.2f}")
+            st.write(f"R-squared score for away goals: {r2_away:.2f}")
 
 elif option == "Whole Season":
     selected_team = st.text_input("Enter the name of the selected team:")
 
     if st.button("Predict Season"):
-            # Function to predict the result of a match
-            def predict_match_result(home_team, away_team):
-                home_team_data = df[df['HomeTeam'] == home_team].tail(1)
-                away_team_data = df[df['AwayTeam'] == away_team].tail(1)
+        # Function to predict the result of a match
+        def predict_match_result(home_team, away_team):
+            home_team_data = df[df['HomeTeam'] == home_team].tail(1)
+            away_team_data = df[df['AwayTeam'] == away_team].tail(1)
 
-                new_data = pd.DataFrame({'AvgHS': home_team_data['AvgHS'].values,
-                                         'AvgAS': away_team_data['AvgAS'].values,
-                                         'AvgHST': home_team_data['AvgHST'].values,
-                                         'AvgAST': away_team_data['AvgAST'].values})
+            new_data = pd.DataFrame({'AvgHS': home_team_data['AvgHS'].values,
+                                     'AvgAS': away_team_data['AvgAS'].values,
+                                     'AvgHST': home_team_data['AvgHST'].values,
+                                     'AvgAST': away_team_data['AvgAST'].values})
 
-                home_goals = round(model_home_goals.predict(new_data)[0])
-                away_goals = round(model_away_goals.predict(new_data)[0])
+            home_goals = round(model_home_goals.predict(new_data)[0])
+            away_goals = round(model_away_goals.predict(new_data)[0])
 
-                if home_goals > away_goals:
-                    return home_team
-                elif home_goals < away_goals:
-                    return away_team
-                else:
-                    return 'DRAW'
+            if home_goals > away_goals:
+                return home_team
+            elif home_goals < away_goals:
+                return away_team
+            else:
+                return 'DRAW'
 
-            # Get the selected team and the list of all teams
-            all_teams = df['HomeTeam'].unique()
+        # Get the selected team and the list of all teams
+        all_teams = df['HomeTeam'].unique()
 
-            # Calculate the result of every possible game for the selected team
-
-            for opponent_team in all_teams:
-                if opponent_team != selected_team:
-                    result = predict_match_result(selected_team, opponent_team)
-                    st.write(f"{selected_team} vs {opponent_team}: {result}")
-                    result2 = predict_match_result(opponent_team, selected_team)
-                    st.write(f"{opponent_team} vs {selected_team}: {result2}")
-
-
+        # Calculate the result of every possible game for the selected team
+        for opponent_team in all_teams:
+            if opponent_team != selected_team:
+                result = predict_match_result(selected_team, opponent_team)
+                st.write(f"{selected_team} vs {opponent_team}: {result}")
+                result2 = predict_match_result(opponent_team, selected_team)
+                st.write(f"{opponent_team} vs {selected_team}: {result2}")
